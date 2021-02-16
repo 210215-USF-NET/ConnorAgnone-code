219040778fa91e660cae6e24e9b4697fbc8d970a
Connor <connoragnone@gmail.com>
2021-02-16 16:03:22 -0500

    Working Prototype

---
+++ 1-csharp/TourOfHeroes/ToHModels/Hero.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ToHModels
+{
+    public class Hero
+    {
+        /// <summary>
+        /// Data structure used for modeling a hero.
+        /// </summary>
+        private string heroName;
+        private int hP;
+        public string HeroName 
+        { 
+            get{return heroName;} 
+            set
+            {
+                if(value.Equals(null)){ } //TODO: throw exception
+                heroName = value;
+            } 
+        }
+        public int HP { get; set; }
+
+        public Element ElementType { get; set; }
+        public SuperPower SuperPower { get; set; }
+    }
+}
