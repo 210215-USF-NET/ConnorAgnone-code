5222dec4437a935a3df42ba1f5c78b88d5e9d481
Connor <connoragnone@gmail.com>
2021-02-24 15:55:12 -0500

    More DB stuff

--- 2-sql/TourOfHeroes/ToHUI/Program.cs
+++ 2-sql/TourOfHeroes/ToHUI/Program.cs
@@ -2,13 +2,30 @@
 using ToHModels;
 using ToHBL;
 using ToHDL;
+using ToHDL.Entities;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+using Microsoft.EntityFrameworkCore;
 namespace ToHUI
 {
     class Program
     {
         static void Main(string[] args)
         {
-            IMenu menu = new HeroMenu(new HeroBL(new HeroRepoFile()));
+            //get the config file
+            var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+            string connectionString = configuration.GetConnectionString("StoreDB");
+            DbContextOptions<StoreDBContext> options = new DbContextOptionsBuilder<StoreDBContext>()
+            .UseSqlServer(connectionString)
+            .Options;
+
+            using var context = new StoreDBContext(options);
+
+            IMenu menu = new HeroMenu(new HeroBL(new HeroRepoDB(context, new HeroMapper())));
             menu.Start();
 
         }
